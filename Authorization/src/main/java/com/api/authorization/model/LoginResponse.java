976aa1ba8b0cc07cf58b6c1423553311c417ef57
Hemanta Sharma <hmnt.sharma092@gmail.com>
2022-06-16 15:06:27 +0530

    initial commit

---
+++ Authorization/src/main/java/com/api/authorization/model/LoginResponse.java
@@ -0,0 +1,18 @@
+package com.api.authorization.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class LoginResponse {
+	private String authToken;
+	private String userId;
+
+}
---
+++ Authorization/src/main/java/com/api/authorization/model/ValidationRespose.java
@@ -0,0 +1,16 @@
+package com.api.authorization.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ValidationRespose {
+	private boolean isValid;
+}
