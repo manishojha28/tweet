c9fe1c077854263590d8b6cc8de699a0da93a8ce
Manish Kumar Ojha <mkomanishojha28@gmail.com>
2022-06-18 18:13:01 +0530

    Update AuthorizationController.java

--- src/main/java/com/api/authorization/controller/AuthorizationController.java
+++ src/main/java/com/api/authorization/controller/AuthorizationController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.api.authorization.model.RegisterRequest;
 import com.api.authorization.entity.Users;
